242559e050891a9bdb2c608a5300bed26b0657c5
uiharuayako <2451602772@qq.com>
2021-05-13 19:10:40 +0800

    预提交

--- src/Network/Sever.java
+++ src/Network/Sever.java
@@ -6,14 +6,7 @@ import java.net.Socket;
 public class Sever implements Runnable {
     // 建立连接
     Socket s = null;
-
-    // 文件相关
-    File file = new File("./服务器/AutoSave.png");
-    int file_size;
     // 文件大小
-    FileOutputStream fos = null;
-    InputStream is = null;
-    byte[] buffer = new byte[4096 * 2];// 缓存
     String comm;// 接受指令
     // 用户相关
     String ID;
@@ -100,37 +93,12 @@ public class Sever implements Runnable {
             //         join$id$你的昵称
             //         stop$id
             if (cho == 1) {
-                myPS.println("stop$" + newID);
+                myPS.println("stop$" + newName);
             } else {
                 myPS.println("join$" + newID + "$" + newName);
             }
             myPS.flush();
             Thread.sleep(50);
-        } catch (Exception e1) {
-            e1.printStackTrace();
-        }
-    }
-
-    void sendImage() {
-        buffer = new byte[4096 * 10];
-        try {
-            File newImage = new File("./服务器/AutoSave.png");
-            FileInputStream fis = new FileInputStream(newImage);
-            byte[] buffer = new byte[4096 * 2];
-            PrintStream myPS = new PrintStream(s.getOutputStream());
-            // 命令格式：sync$id$文件长度$当前工具
-            //         join$id$你的昵称
-            //         stop$id
-            myPS.println("sync$" + ID + "$" + fis.available() + "$" + "end");
-            myPS.flush();
-            Thread.sleep(100);
-            OutputStream os = s.getOutputStream();
-            int size = 0;
-            while ((size = fis.read(buffer)) != -1) {
-                os.write(buffer, 0, size);
-                os.flush();
-            }
-            Thread.sleep(20);
         } catch (Exception e1) {
             e1.printStackTrace();
         }
